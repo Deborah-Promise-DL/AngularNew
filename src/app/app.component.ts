488d648e452babe9ab781e62a5ea52244bb8c48f
DeborahPromise <deborahpromise1@gmail.com>
2025-03-27 11:39:08 -0700

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DatabindingComponent,FooterComponent,SidebarComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { SettingsComponent } from './settings/settings.component';
+import { UserslistComponent } from './userslist/userslist.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "registration",
+        component: RegistrationComponent
+    },
+    {
+        path: "navbar",
+        component: NavbarComponent
+    },
+    {
+        path: "sidebar",
+        component: SidebarComponent
+    },
+    {
+        path: "contactus",
+        component: ContactusComponent
+    },
+    {
+        path: "settings",
+        component: SettingsComponent
+    },
+    {
+        path: "userslist",
+        component: UserslistComponent
+    }
+      
+];
